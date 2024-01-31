96cda33245f66a873be5bbca6416d18cdaff63ca
Jamie Ingalls <jamesmaclay@gmail.com>
2024-01-31 13:53:21 -0500

    Header back in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.scss";
 // import * as Tone from "tone";
-// import Header from "./components/Header/Header";
+import Header from "./components/Header/Header";
 import DrumGrid from "./components/Drums/DrumGrid";
 import SynthGrid from "./components/Synth/SynthGrid";
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
       <DrumGrid />
      <SynthGrid/>
     </div>
