40eed3ee9721f602f3877b535792a4425ee74dfa
Jamie Ingalls <jamesmaclay@gmail.com>
2024-01-30 21:29:12 -0500

    file cleanup

--- src/components/Grid/DrumGrid.js
+++ src/components/Grid/DrumGrid.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Button from "../Button/Button";
-import Drums from "../Instruments/MembraneSynth";
+import Drums from "../Instruments/Drums";
 import * as Tone from "tone";
 import "./Grid.scss";
 
