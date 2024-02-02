0d8b1971e66748ca752fb0439a39b7e991de306f
Jamie Ingalls <jamesmaclay@gmail.com>
2024-02-01 19:08:37 -0500

    Header Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.scss";
 import Sequencer from "./components/Sequencer/Sequencer";
-
+import Header from "./components/Header/Header";
 function App( ) {
  
 
   return (
     <>
+      <Header/>
       <Sequencer/>
     </>
    
