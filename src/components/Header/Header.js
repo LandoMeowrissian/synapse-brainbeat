5205b1c1debefdce8037b72b032d47a624058bc1
Jamie Ingalls <jamesmaclay@gmail.com>
2024-02-04 15:04:48 -0500

    if nothing else we can turn this in

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
 import "./Header.scss";
-
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="header__container">
+      <Link to={"/"}>
       <h1 className="header"> synapse / brainbeat</h1>
+      </Link>
+      
     </div>
   );
 };
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <div className="login">
-      <h2 className="login-heading">join us</h2>
+      <h2 className="login-heading">sign up / login</h2>
       <form className="login-form">
-        <label id="username">username</label>
-        <input placeholder="username"></input>
-        <label id="email">email</label>
-        <input placeholder="example@email.com"></input>
-        <label id="pwd">password</label>
-        <input placeholder="must be at least 8 characters"></input>
-        <label id="confirm-pwd">confirm password</label>
-        <input placeholder="confirm password"></input>
+        <label id="username">enter / username</label>
+        <input name="login" placeholder="username"></input>
+        <label id="email">enter / email</label>
+        <input name="login" placeholder="example@email.com"></input>
+        <label id="pwd">enter / password</label>
+        <input name="login" placeholder="must be at least 8 characters"></input>
+        <label id="confirm-pwd">enter / confirm password</label>
+        <input name="login" placeholder="confirm password"></input>
         <Link to={"/player"}>
-          <button className="login-button">make noise</button>
+          <button className="login-button">make / noise</button>
         </Link>
       </form>
     </div>
