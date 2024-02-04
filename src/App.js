8c9dc4828eecab24f7664e9e7193bc00d0b7ba4e
Jamie Ingalls <jamesmaclay@gmail.com>
2024-02-04 09:44:42 -0500

    Browser Router, Home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.scss";
-import Sequencer from "./components/Sequencer/Sequencer";
-import Header from "./components/Header/Header";
-function App( ) {
- 
+// import Sequencer from "./components/Sequencer/Sequencer";
+// import Header from "./components/Header/Header";
+import HomePage from "./pages/HomePage/";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+function App() {
   return (
-    <>
-      <Header/>
-      <Sequencer/> 
-    </>
-   
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<HomePage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header/Header";
+import Login from "../components/Login/Login";
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <Login />
+    </>
+  )
+}
+
+export default HomePage;
