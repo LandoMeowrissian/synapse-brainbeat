d24e03effeaef8312e100b81c51732d1a7451a27
Jamie Ingalls <jamesmaclay@gmail.com>
2024-02-07 17:57:50 -0500

    interval and interation parameters added

--- src/components/Looper/Looper.js
+++ src/components/Looper/Looper.js
@@ -3,29 +3,37 @@ import { useState } from "react";
 
 const Looper = () => {
   const [started, setStarted] = useState(false);
-  const bassSynth = new Tone.MembraneSynth({
+  const kick = new Tone.MembraneSynth({
     pitchDecay: 0.05
   }).toDestination();
 
-  const loop = (time) => {
-    bassSynth.triggerAttackRelease("A0", "8n", time);
+  const playKick = (time) => {
+    kick.triggerAttackRelease("A0", "4n", time);
+    console.log(time)
   };
 
-  const interval = "16n";
-  const steps = new Tone.Loop(loop, interval); // 16n === notes per measure
+  const synth = new Tone.AMSynth().toDestination();
+
+  const playSynth = (time) => {
+    synth.triggerAttackRelease("A4", "4n", time);
+  }
+ 
+  const interval = "8n";
+  const iterations = 2;
+  const steps = new Tone.Loop(playKick, interval); // 16n === notes per measure
 
   const startAudio = async () => {
     await Tone.Transport.start();
     steps.start(0);
     setStarted(true);
-    console.log(steps);
+    console.log('started', interval);
   };
 
   const stopAudio = () => {
     Tone.Transport.stop();
     Tone.Transport.cancel();
     setStarted(false);
-    console.log("stopped running");
+    console.log("stopped");
   };
 
   return (
